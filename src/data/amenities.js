0df9e7e693f010e519ed1d9d6e4b63e2483d13e0
Joao Bravo <joao.bravo@zeneducate.com>
2026-04-18 11:46:22 +0100

    fix: correct amenity count in translations and add FA prefix to icon classes

--- src/data/amenities.js
+++ src/data/amenities.js
@@ -1,16 +1,16 @@
 const AMENITIES = [
-  { icon: 'fa-kitchen-set', key: 'a1' },
-  { icon: 'fa-wifi', key: 'a2' },
-  { icon: 'fa-tv', key: 'a3' },
-  { icon: 'fa-laptop', key: 'a4' },
-  { icon: 'fa-shirt', key: 'a5' },
-  { icon: 'fa-fire', key: 'a6' },
-  { icon: 'fa-chair', key: 'a7' },
-  { icon: 'fa-leaf', key: 'a8' },
-  { icon: 'fa-car', key: 'a9' },
-  { icon: 'fa-shower', key: 'a10' },
-  { icon: 'fa-lock', key: 'a11' },
-  { icon: 'fa-sun', key: 'a12' },
+  { icon: 'fa fa-kitchen-set', key: 'a1' },
+  { icon: 'fa fa-wifi', key: 'a2' },
+  { icon: 'fa fa-tv', key: 'a3' },
+  { icon: 'fa fa-laptop', key: 'a4' },
+  { icon: 'fa fa-shirt', key: 'a5' },
+  { icon: 'fa fa-fire', key: 'a6' },
+  { icon: 'fa fa-chair', key: 'a7' },
+  { icon: 'fa fa-leaf', key: 'a8' },
+  { icon: 'fa fa-car', key: 'a9' },
+  { icon: 'fa fa-shower', key: 'a10' },
+  { icon: 'fa fa-lock', key: 'a11' },
+  { icon: 'fa fa-sun', key: 'a12' },
 ];
 
 export default AMENITIES;
